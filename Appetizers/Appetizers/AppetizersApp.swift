e91333ea747f5f053e79d2bde6af608ade40ef92
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2024-02-12 12:50:28 +0300

    0.0
    Hello World

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ali Durna on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appetizers/Appetizers/ContentView.swift
@@ -0,0 +1,23 @@
+//  ContentView.swift
+//  Appetizers
+//
+//  Created by Ali Durna on 12.02.2024.
+//Hello World
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
